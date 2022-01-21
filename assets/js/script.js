ac856b75deffbec0b804a8b6291a778f8dd0c568
JearlDev <94915833+JearlDev@users.noreply.github.com>
2022-01-21 16:50:46 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Expand functionality
 let expandBtn = document.querySelector('.expandBtn');
 
 expandBtn.addEventListener('click', expand);
@@ -5,7 +6,6 @@ expandBtn.addEventListener('click', expand);
 function expand() {
   document.querySelector('.quoteSection').classList.toggle('hide');
   document.querySelector('.expandedSection').classList.toggle('hide');
-  // document.querySelector('.expandedSection').classList.toggle('expandActive2');
   document.querySelector('.fa-chevron-circle-down').classList.toggle('fa-flip-vertical');
   document.querySelector('.clockSection').classList.toggle('expandActive');
   
@@ -16,6 +16,25 @@ function expand() {
   } else {
     btnText.innerText = 'more';
   }
-    
 }
 
+
+//Quote functionallity
+window.addEventListener('load', displayQuote)
+
+document.querySelector('.refreshBtn')
+.addEventListener('click', displayQuote);
+
+async function getQuote(){
+  const res = await axios.get('https://api.quotable.io/random');
+  const quote = res.data.content;
+  const author = res.data.author;
+  return [quote, author];
+}
+
+async function displayQuote(){
+  const quoteContent = await getQuote();
+
+  document.querySelector('.quote').innerHTML = `${quoteContent[0]}`;
+  document.querySelector('.author').innerHTML = `${quoteContent[1]}`;
+}
